92eca16a77e394079b4603a08e03ea9b5d771d1d
jdedek <dedekj@gmail.com>
2012-09-03 21:00:47 +0200

    online continues

--- src/czsem/resources/Treex/treex_online.pl
+++ src/czsem/resources/Treex/treex_online.pl
@@ -8,15 +8,21 @@ use Treex::Block::CzsemRpcReader;
 use Frontier::Daemon;
 use Net::Address::IP::Local;
 use Sys::Hostname;
+use Treex::Core::Log;
+use Scalar::Util qw(reftype);
+
+
+Treex::Core::Log::log_set_error_level('INFO');
 
 
 
 my $port_number = 9090;
+my $isReady = 0;
+my $scenario = undef;
 
-sub start_server
+sub startServer
 {
-    my $methods;
-    $methods = {
+    my $methods = {
       'treex.isReady' => \&isReady,
       'treex.analyzeDoc' => \&analyzeDoc,
       'treex.terminate' => \&terminate };
@@ -31,7 +37,6 @@ sub start_server
         or die "Couldn't start HTTP server: $!";
 }
 
-my $isReady = undef;
 
 sub isReady
 {
@@ -43,7 +48,11 @@ sub terminate
     exit;
 }
 
-my $scenario = Treex::Core::Scenario->new(from_string => 'Util::SetGlobal language=en CzsemRpcReader W2A::Segment W2A::Tokenize');
+sub initScenario
+{
+  $scenario = Treex::Core::Scenario->new(from_string => 'Util::SetGlobal language=en CzsemRpcReader W2A::EN::Segment devel\analysis\en\s_w2t.scen');
+}
+
 
 
 sub backup {
@@ -59,23 +68,32 @@ sub backup {
   		$child->set_parent($predicate);
   	}
 	
-	$doc->save('demo.treex');
+	$doc->save('C:\workspace\demo.treex');
 
 }
 
 sub analyzeDoc
 {
- 	
   my $doc = Treex::Core::Document->new;
   
   my $zone = $doc->create_zone('en');
-  $zone->set_text("Hallo world!"); 	
+  $zone->set_text("Hallo world! Life is great, isn't it? John is the man who died in London recently."); 	
  
 	
 	$Treex::Block::CzsemRpcReader::dataQueue->enqueue($doc);  
   $Treex::Block::CzsemRpcReader::dataQueue->enqueue(undef);  
   $scenario->run;
   
+  # DEBUG !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ 	$doc->save('C:\workspace\demo.treex');
+  # DEBUG !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+}
+
+
+sub encodeDoc
+{
+  my $doc = shift;
+
   foreach my $bundle ( $doc->get_bundles ) {
     foreach my $bundlezone ( $bundle->get_all_zones ) {
       print STDERR "-----------sentence--------\n";      
@@ -89,8 +107,12 @@ sub analyzeDoc
           foreach my $path ( $node->attribute_paths ) {
             my $value = $node->attr($path);
             my $valueAll = $node->all($path);
+            my $valueType = reftype($value);
+            
+            if (! $valueType) {$valueType=""}; 
+            
             if ($value) { 
-              print STDERR "$path = $value ($valueAll)\n";
+              print STDERR "$path = $value [$valueType] ($valueAll)\n";
             } 
           }
           print STDERR "parent: ";
@@ -101,15 +123,21 @@ sub analyzeDoc
       }
     }
   }
-	
+  	
 	return { from=>{surname=>"dedek"} };
 }
  
+#initScenario;
 
- 	
-start_server; 
+#analyzeDoc;
+
+#startServer;
+
+my $doc = Treex::Core::Document->new( { filename => 'C:\workspace\demo.treex' } ); 
+encodeDoc($doc);
 
 print STDERR "treex online end\n";
 
+exit;
 
- 
+
