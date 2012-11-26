0e4766f6149763e2eac154701742f87be6efa74b
jdedek <dedekj@gmail.com>
2012-11-26 10:59:19 +0100

    run treex server sh finished

--- src/czsem/resources/Treex/run_treex_server.sh
+++ src/czsem/resources/Treex/run_treex_server.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+
+if test ! -n "$TREEX_ROOT"
+then
+  TREEX_ROOT=`which treex | sed -e 's/bin\/treex$//'`
+fi
+
+if test ! -n "$TREEX_ONLINE"
+then
+  CURRENT_SCRIPT=`readlink -f $0`
+  TREEX_ONLINE=`echo $CURRENT_SCRIPT | sed -e 's/\/[^\/]*$/\/treex_online.pl/'`
+fi
+
  
-cd /home/developer/treex
-perl /home/developer/czsem/src/czsem/resources/Treex/treex_online.pl
+echo "Running perl $TREEX_ONLINE $@"
+echo "in directory: $TREEX_ROOT"
+
+cd $TREEX_ROOT
+perl $TREEX_ONLINE $@
+
+
