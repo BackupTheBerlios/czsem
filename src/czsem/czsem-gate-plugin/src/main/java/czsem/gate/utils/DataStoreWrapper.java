16bf7a9d81f52f4c2a646e07e30d21dc219945b4
jdedek <dedekj@gmail.com>
2013-09-16 10:12:54 +0200

    DataStoreWrapper - search by resource id

--- src/czsem/czsem-gate-plugin/src/main/java/czsem/gate/utils/DataStoreWrapper.java
+++ src/czsem/czsem-gate-plugin/src/main/java/czsem/gate/utils/DataStoreWrapper.java
@@ -13,6 +13,7 @@ import gate.security.SecurityException;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class DataStoreWrapper {
@@ -45,10 +46,20 @@ public class DataStoreWrapper {
 		}
 	}
 	
+	
+	private static final String LR_PERSISTENCE_ID_SUFFIX = "___" + "0" + "___" + ".ser";
+	
 	public static String lrNameToLRPersistenceId(String lrName) {
 		//return lrName + "___" + new Date().getTime() + "___" + random();
 		
-		return lrName + "___" + "0" + "___" + ".ser";
+		return lrName + LR_PERSISTENCE_ID_SUFFIX;
+	}
+
+	public static String lRPersistenceIdToLrName(String lRPersistenceId) {
+		if (lRPersistenceId.endsWith(LR_PERSISTENCE_ID_SUFFIX))
+			return lRPersistenceId.substring(0, lRPersistenceId.length() - LR_PERSISTENCE_ID_SUFFIX.length());
+		
+		return null;
 	}
 
 	public void persistDoc(Document gateDoc) throws PersistenceException, SecurityException {
@@ -57,20 +68,33 @@ public class DataStoreWrapper {
 		ds.sync(gateDoc);
 		
 	}
+	
+	public Document loadDoc(String docLrId) throws ResourceInstantiationException {
+		return (Document) GateUtils.loadResourceFormDatastore(ds, DocumentImpl.class.getCanonicalName(), docLrId);
+	}
 
 	public boolean containsGateDocument(String fileName) {
 	    return containsResource(DocumentImpl.class, fileName);
 	}
 
 	public boolean containsResource(Class<? extends LanguageResource> c, String fileName) {
+		return containsLrId(c, lrNameToLRPersistenceId(fileName)); 
+	}
+
+	public boolean containsDocumentLrId(String lrId) {
+		return containsLrId(DocumentImpl.class, lrId);
+	}
+	
+	public boolean containsLrId(Class<? extends LanguageResource> c, String lrId) { 
 	    File resourceTypeDir = new File(ds.getStorageDir(), c.getCanonicalName());
 	    
 	    if(! resourceTypeDir.exists()) return false;
 	    
-	    File resourceFile = new File(resourceTypeDir, lrNameToLRPersistenceId(fileName)); 
+	    File resourceFile = new File(resourceTypeDir, lrId); 
 
 	    return resourceFile.exists();
 	}
+
 
 	public void close() throws PersistenceException {
 		ds.close();
@@ -108,6 +132,19 @@ public class DataStoreWrapper {
 		}
 		
 	}
+	
+	public List<String> getDocumentLrIds() throws PersistenceException {
+		return getLrIds(DocumentImpl.class);
+	}
+
+	public List<String> getLrIds(Class<? extends LanguageResource> cls) throws PersistenceException {
+		
+		@SuppressWarnings("unchecked")
+		List<String> ret = ds.getLrIds(cls.getCanonicalName());
+		
+		return ret;
+	}
+	
 
 	public Iterable<? extends Document> iterateAllDocuments() {
 		return new Iterable<DocumentImpl>() {
